519d7fcc146653e73c5997f9f70f1895131b4493
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-03-23 02:58:28 +0300

    Done event listeners

--- script.js
+++ script.js
@@ -32,81 +32,24 @@ function operate(operator, num1, num2) {
   }
 }
 
-let num1;
-let num2;
-let operator;
-let array = [];
+const display = document.querySelector(".display");
+const buttons = document.querySelector(".buttons");
 
-const displayLive = document.querySelector(".display");
-const buttonsNum = document.querySelectorAll(".num");
-const operators = document.querySelectorAll(".operator");
-const equal = document.querySelector(".equal");
-const ac = document.querySelector(".ac");
-const c = document.querySelector(".c");
+console.log(display.textContent);
+console.log(buttons);
 
-// Displays the the clicked numbers on the display area.
-displayLive.textContent = "0";
+let displayValue = "0";
+updateDisplay();
 
-function display() {
-  let displayValue;
-  buttonsNum.forEach((button) =>
-    button.addEventListener("click", () => {
-      // Clears display in multiple operations. Like 2+5-4*8.
-      if (array.length == 0) {
-        displayLive.textContent = "";
-      } else if (array.length > 10) {
-        alert("Too much number.");
-        clearAll();
-      } else if (array.includes(".") && button.textContent == ".") {
-        alert("Only one dot allowed, in a number");
-        clearAll();
-      }
-
-      displayLive.textContent += button.innerText;
-      array.push(button.innerText);
-    })
-  );
+function updateDisplay() {
+  display.textContent = displayValue;
 }
 
-display();
-
-operators.forEach((op) => {
-  op.addEventListener("click", () => {
-    if (!num1) {
-      num1 = Number(array.join(""));
-      operator = op.innerText;
-      displayLive.textContent = "";
-      array = [];
-    } else {
-      // Clears display in multiple operations. Like 2+5-4*5.
-      if (array.length == 0) {
-        displayLive.textContent = "";
-      }
-      num2 = Number(array.join(""));
-      array = [];
-      num1 = operate(operator, num1, num2);
-      displayLive.textContent = num1;
-      operator = op.innerText;
-    }
-  });
-});
-
-equal.addEventListener("click", () => {
-  num2 = Number(array.join(""));
-  num1 = operate(operator, num1, num2); // In case user click another operator after the equal.
-  displayLive.textContent = num1;
-  array = [];
-});
-
-function clearAll() {
-  window.location.reload();
-}
-
-// Clears all window when ac cliked
-ac.addEventListener("click", clearAll);
-
-// Clear last input
-c.addEventListener("click", () => {
-  array = array.slice(0, -1);
-  displayLive.textContent = array.join("");
+buttons.addEventListener("click", function (e) {
+  if (!e.target.matches("button")) return;
+  //   console.log(e.target.classList);
+  if (e.target.classList.contains("operator")) {
+    console.log(e.target.value);
+    return;
+  }
 });
