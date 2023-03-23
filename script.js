cd8b0ee8219f4c54bff7b2306376f0b427ebcf0d
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-03-23 05:28:26 +0300

    Bug fix

--- script.js
+++ script.js
@@ -29,16 +29,19 @@ function operate(operator, num1, num2) {
     return multiply(num1, num2);
   } else if (operator == "÷") {
     return divide(num1, num2);
+  } else {
+    return num2;
   }
 }
 
 const display = document.querySelector(".display");
 const buttons = document.querySelector(".buttons");
 
-console.log(display.textContent);
-console.log(buttons);
-
 let displayValue = "0";
+let firstValue = null;
+let operator = null;
+let waitForValue = null;
+
 updateDisplay();
 
 function updateDisplay() {
@@ -47,9 +50,77 @@ function updateDisplay() {
 
 buttons.addEventListener("click", function (e) {
   if (!e.target.matches("button")) return;
-  //   console.log(e.target.classList);
+  //   console.log(e.target.textContent);
   if (e.target.classList.contains("operator")) {
-    console.log(e.target.value);
+    // console.log(e.target.value);
+    startToOperate(e.target.textContent);
+    updateDisplay();
+
     return;
   }
+  if (e.target.classList.contains("dot")) {
+    inputDot();
+    updateDisplay();
+    // console.log(e.target.value);
+    return;
+  }
+  if (e.target.classList.contains("ac")) {
+    // console.log(e.target.value);
+    resetDisplay();
+    updateDisplay();
+    return;
+  }
+  if (e.target.classList.contains("c")) {
+    // console.log(e.target.value);
+    resetDigit();
+    updateDisplay();
+    return;
+  }
+
+  inputNumber(e.target.textContent);
+  updateDisplay();
 });
+
+function inputNumber(value) {
+  if (waitForValue === true) {
+    displayValue = value;
+    waitForValue = false;
+  } else {
+    displayValue = displayValue == "0" ? value : (displayValue += value);
+  }
+}
+
+function inputDot() {
+  if (!displayValue.includes(".")) {
+    displayValue += ".";
+  }
+}
+
+function resetDisplay() {
+  displayValue = "0";
+}
+
+function resetDigit() {
+  displayValue = displayValue.slice(0, -1);
+}
+
+function startToOperate(op) {
+  let value = parseFloat(displayValue);
+
+  if (operator && waitForValue) {
+    operator = op;
+    return;
+  }
+
+  if (firstValue === null) {
+    firstValue = value;
+  } else if (operator) {
+    let result = operate(operator, firstValue, value);
+    firstValue = result;
+    displayValue = String(result);
+  }
+
+  waitForValue = true;
+  operator = op;
+  console.log(displayValue, firstValue, operator, waitForValue);
+}
